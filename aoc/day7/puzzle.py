0fb367687551a1d7459c732371ada0fa1540a9e0
Kalle Lämsä <1397804+coocos@users.noreply.github.com>
2023-12-07 20:02:50 +0200

    Solve day 7 part 2

--- aoc/day7/puzzle.py
+++ aoc/day7/puzzle.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from collections import Counter
 from dataclasses import dataclass, field
 from enum import Enum
@@ -14,8 +15,31 @@ class Type(Enum):
     FOUR_OF_A_KIND = 6
     FIVE_OF_A_KIND = 7
 
+    @staticmethod
+    def from_raw_hand(hand: str) -> Type:
+        counts = Counter(hand)
+        hand_type = Type.HIGH_CARD
+        for count in counts.values():
+            if count == 5:
+                return Type.FIVE_OF_A_KIND
+            elif count == 4:
+                return Type.FOUR_OF_A_KIND
+            elif count == 3:
+                if hand_type == Type.ONE_PAIR:
+                    hand_type = Type.FULL_HOUSE
+                else:
+                    hand_type = Type.THREE_OF_A_KIND
+            elif count == 2:
+                if hand_type == Type.ONE_PAIR:
+                    hand_type = Type.TWO_PAIR
+                elif hand_type == Type.THREE_OF_A_KIND:
+                    hand_type = Type.FULL_HOUSE
+                else:
+                    hand_type = Type.ONE_PAIR
+        return hand_type
 
-card_rankings = {c: i for i, c in enumerate("AKQJT98765432")}
+
+CARD_RANKINGS = {c: i for i, c in enumerate("AKQJT98765432")}
 
 
 @dataclass
@@ -25,34 +49,26 @@ class Hand:
     hand_type: Type = field(init=False)
 
     def __post_init__(self):
-        self.hand_type = Type.HIGH_CARD
+        self.hand_type = Type.from_raw_hand(self.raw)
+
+    def apply_joker(self):
         counts = Counter(self.raw)
-        for count in counts.values():
-            if count == 5:
-                self.hand_type = Type.FIVE_OF_A_KIND
-                break
-            elif count == 4:
-                self.hand_type = Type.FOUR_OF_A_KIND
-                break
-            elif count == 3:
-                if self.hand_type == Type.ONE_PAIR:
-                    self.hand_type = Type.FULL_HOUSE
-                else:
-                    self.hand_type = Type.THREE_OF_A_KIND
-            elif count == 2:
-                if self.hand_type == Type.ONE_PAIR:
-                    self.hand_type = Type.TWO_PAIR
-                elif self.hand_type == Type.THREE_OF_A_KIND:
-                    self.hand_type = Type.FULL_HOUSE
-                else:
-                    self.hand_type = Type.ONE_PAIR
+        if (joker_count := counts.get("J")) and joker_count != 5:
+            best_joker = next(
+                card
+                for card, _ in sorted(
+                    counts.items(), key=lambda count: count[1], reverse=True
+                )
+                if card != "J"
+            )
+            self.hand_type = Type.from_raw_hand(self.raw.replace("J", best_joker))
 
     def __lt__(self, other):
         if self.hand_type.value != other.hand_type.value:
             return self.hand_type.value < other.hand_type.value
         for a, b in zip(self.raw, other.raw):
             if a != b:
-                return card_rankings[a] > card_rankings[b]
+                return CARD_RANKINGS[a] > CARD_RANKINGS[b]
 
 
 def parse_input() -> Iterable[Hand]:
@@ -62,13 +78,23 @@ def parse_input() -> Iterable[Hand]:
 
 
 def part_1(hands: list[Hand]) -> int:
-    return sum(i * hand.bid for i, hand in enumerate(sorted(hands), 1))
+    return sum(rank * hand.bid for rank, hand in enumerate(sorted(hands), 1))
+
+
+def part_2(hands: list[Hand]) -> int:
+    global CARD_RANKINGS
+    CARD_RANKINGS = {c: i for i, c in enumerate("AKQT98765432J")}
+    for hand in hands:
+        hand.apply_joker()
+    return sum(rank * hand.bid for rank, hand in enumerate(sorted(hands), 1))
 
 
 def solve() -> None:
     hands = list(parse_input())
 
     assert part_1(hands) == 246912307
+
+    assert part_2(hands) == 246894760
 
 
 if __name__ == "__main__":
