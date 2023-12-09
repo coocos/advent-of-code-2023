98c549902e4df9031409a6403cc6d12a4b81f70f
Kalle Lämsä <1397804+coocos@users.noreply.github.com>
2023-12-09 08:44:32 +0200

    Solve day 9 part 2

--- aoc/day9/puzzle.py
+++ aoc/day9/puzzle.py
@@ -9,15 +9,34 @@ def parse_input() -> list[list[int]]:
     return histories
 
 
+def differences(history: list[int]) -> list[list[int]]:
+    differences = [history]
+    while any(num != 0 for num in differences[-1]):
+        last_seq = differences[-1]
+        next_seq = [b - a for a, b in itertools.pairwise(last_seq)]
+        differences.append(next_seq)
+    return differences
+
+
 def part_1(histories: list[list[int]]) -> int:
     extrapolated_sum = 0
     for history in histories:
-        differences = [history]
-        while any(num != 0 for num in differences[-1]):
-            last_seq = differences[-1]
-            next_seq = [b - a for a, b in itertools.pairwise(last_seq)]
-            differences.append(next_seq)
-        extrapolated_sum += sum(diff[-1] for diff in differences)
+        prev = 0
+        for diff in reversed(differences(history)):
+            diff.append(diff[-1] + prev)
+            prev = diff[-1]
+        extrapolated_sum += prev
+    return extrapolated_sum
+
+
+def part_2(histories: list[list[int]]) -> int:
+    extrapolated_sum = 0
+    for history in histories:
+        prev = 0
+        for diff in reversed(differences(history)):
+            diff.insert(0, diff[0] - prev)
+            prev = diff[0]
+        extrapolated_sum += prev
     return extrapolated_sum
 
 
@@ -25,6 +44,8 @@ def solve() -> None:
     histories = parse_input()
 
     assert part_1(histories) == 1696140818
+
+    assert part_2(histories) == 1152
 
 
 if __name__ == "__main__":
